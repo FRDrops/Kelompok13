de7d6899b165de24249b8cbb4d53399391c1737f
yocepp <yosefarg007@gmail.com>
2023-06-25 14:18:31 +0700

    update class fillingtopping pt2

--- Kelompok 13 UAS PBO 2023/src/kelompok/pkg13/uas/pbo/pkg2023/FillingTopping.java
+++ Kelompok 13 UAS PBO 2023/src/kelompok/pkg13/uas/pbo/pkg2023/FillingTopping.java
@@ -8,6 +8,23 @@ package kelompok.pkg13.uas.pbo.pkg2023;
  *
  * @author ASUS
  */
-public class FillingTopping {
-    int var = 1;
+public abstract class FillingTopping {
+    
+    public abstract double hargaKeju();
+
+    public abstract double hargaCoklat();
+
+    public abstract double hargaVanilla();
+
+    public abstract double hargaSelaiRedBean();
+
+    public abstract double hargaSosis();
+
+    public abstract double hargaSmokedBeef();
+
+    public abstract double hargaBawangBombay();
+
+    public abstract double totalHargaFilling();
+
+    public abstract double totalHargaTopping();
 }
