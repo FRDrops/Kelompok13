dc7ba66db77d7130f0474679a9539a36d728ca57
FRDrops <rnispgjk@gmail.com>
2023-06-25 14:38:23 +0700

    update
    HargaKalkulasi

--- Kelompok 13 UAS PBO 2023/src/Interfaceses/HargaKomposisi.java
+++ Kelompok 13 UAS PBO 2023/src/Interfaceses/HargaKomposisi.java
@@ -1,8 +1,10 @@
+package Interfaceses;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package Interfaceses;
+
 
 /**
  *
--- Kelompok 13 UAS PBO 2023/src/kelompok/pkg13/uas/pbo/pkg2023/KalkulasiHarga.java
+++ Kelompok 13 UAS PBO 2023/src/kelompok/pkg13/uas/pbo/pkg2023/KalkulasiHarga.java
@@ -9,8 +9,25 @@ package kelompok.pkg13.uas.pbo.pkg2023;
  * @author ASUS
  */
 public class KalkulasiHarga {
-    
-    public void harga (){
-        System.out.println("Heelo world");
+    int var1;
+    int var2;
+    int var3;
+    int a;
+    int b;
+    int c;
+    int d;
+    int akg;
+    int bkg;
+    int ckg;
+    void JumlahRoti(){
+        a = akg/50;
+        b = bkg/400;
+        c = ckg/190;
+    }
+    void TotalHargaU(){
+       d = d*140/100;
+    }
+    void Modal(){
+        d = a*var1 + b*var2 + c*var3;
     }
 }
